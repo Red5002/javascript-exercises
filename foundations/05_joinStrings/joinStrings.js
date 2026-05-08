10458c772b67f1c7a220d920d9549079aad5d3de
elijah <amujoelijah3@gmail.com>
2026-05-08 23:03:21 +0100

    added let to variable names, and finished exercise 6

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,13 +8,13 @@
 */
 
 // Add your code right below, good luck!
-firstName = 'Carlos'
-lastName = 'Stevenson'
-thisYear = 1965
-birthYear = 1947
-greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`
-fullName = `${firstName} ${lastName}`
-age = thisYear - birthYear
+let firstName = 'Carlos'
+let lastName = 'Stevenson'
+let thisYear = 1965
+let birthYear = 1947
+let greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`
+let fullName = `${firstName} ${lastName}`
+let age = thisYear - birthYear
 
 
 
